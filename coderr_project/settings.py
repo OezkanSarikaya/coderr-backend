d5597df974ade01dadf4dadd6c51f48b9a49a388
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-12-18 10:51:59 +0000

    made app ready for server

--- coderr_project/settings.py
+++ coderr_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.69.148.178','127.0.0.1','coderr-backend.oezkan-sarikaya.de']
 
 
 # Application definition
